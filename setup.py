a1d150f5b362478d2c1b1764ac7b54ec53088780
Hamed <mehrabi.hamed@outlook.com>
2023-03-06 08:16:11 +0000

    Update requirements and setup files

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ with open("README.md", "r", encoding="utf-8") as f:
     long_description = f.read()
 
 ## edit below variables as per your requirements -
-REPO_NAME = "REPO_NAME"
-AUTHOR_USER_NAME = "c17hawke"
+REPO_NAME = "Project-DVC-NLP-StackOverflow"
+AUTHOR_USER_NAME = "Hamed Mehrabi"
 SRC_REPO = "src"
 LIST_OF_REQUIREMENTS = []
 
@@ -14,13 +14,13 @@ setup(
     name=SRC_REPO,
     version="0.0.1",
     author=AUTHOR_USER_NAME,
-    description="A small package for DVC",
+    description="A small Project for DVC NLP-Stack Overflow",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
-    author_email="sunny.c17hawke@gmail.com",
+    url=f"https://github.com/hamehrabi/Project-DVC-NLP-StackOverflow",
+    author_email="mehrabi.hamed@outlook.com",
     packages=[SRC_REPO],
     license="MIT",
-    python_requires=">=3.6",
+    python_requires=">=3.8",
     install_requires=LIST_OF_REQUIREMENTS
 )
